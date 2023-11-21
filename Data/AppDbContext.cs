a8e71e043fb30f3e53b4cb52016492cbb69b566e
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-11-21 14:33:29 +0100

    Succeded Migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GrpcService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrpcService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> context) : base(context) { }
+
+        public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using GrpcService.Data;
 using GrpcService.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+//Database Connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"));
+});
+
 
 // Add services to the container.
 builder.Services.AddGrpc();
